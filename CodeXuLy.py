cda180121a5a39ba3ba098c859a5e77439bb48e5
LE NGUYEN NGOC LINH <118876372+LINHLNN24042004@users.noreply.github.com>
2023-04-15 19:01:06 +0700

    Linh sửa code chỗ tủ

--- CodeXuLy.py
+++ CodeXuLy.py
@@ -10,44 +10,58 @@ class Tu:  # Lop Tu
         self.trang_thai = trang_thai
 
 
-class QuanLyTuDo:  # Lop Quan Li Tu Do
-    def __init__(self, danh_sach_tu):
-        self.danh_sach_tu = danh_sach_tu
+class QuanLyTuDo:
+    def __init__(self):
+        self.danh_sach_tu_trong = []  # Danh sách tủ trống
+        self.danh_sach_tu_da_dat = []  # Danh sách tủ đã đặt
 
-    def hien_thi_so_tu_con_trong(self):
-        so_tu_con_trong = sum(tu.trang_thai == "trong" for tu in self.danh_sach_tu)
-        return so_tu_con_trong
-        #print(f"Số tủ còn trống: {so_tu_con_trong}")
+    def them_tu_trong(self, tu):
+        if tu.trang_thai == "trống":
+            self.danh_sach_tu_trong.append(tu)  # Thêm tủ trống vào danh sách tủ trống
+
+    def them_tu_da_dat(self, tu):
+        if tu.trang_thai == "đã đặt":
+            return
+        self.danh_sach_tu_da_dat.append(tu)  # Thêm tủ đã đặt vào danh sách tủ đã đặt
+
+    def xoa_tu_trong(self, ma_tu):
+        for tu in self.danh_sach_tu_trong:
+            if tu.ma_tu == ma_tu:
+                self.danh_sach_tu_trong.remove(tu)
+                break
+
+    def xoa_tu_da_dat(self, ma_tu):
+        for tu in self.danh_sach_tu_da_dat:
+            if tu.ma_tu == ma_tu:
+                self.danh_sach_tu_da_dat.remove(tu)
+                break
 
     def hien_thi_tu_trong(self):
-        print("Các mã tủ còn trống:")
-        for tu in self.danh_sach_tu:
-            if tu.trang_thai == "trong":
-                print(tu.ma_tu)
+        print("Danh sách tủ trống:")
+        for tu in self.danh_sach_tu_trong:
+            print(tu.ma_tu)
 
     def hien_thi_tu_da_dat(self):
-        print("Tủ bạn đang sử dụng là tủ nào:")
-        for tu in self.danh_sach_tu:
-            if tu.trang_thai == "Đã đặt":
-                print(tu.ma_tu)
+        print("Danh sách tủ đã đặt:")
+        for tu in self.danh_sach_tu_da_dat:
+            print(tu.ma_tu)
 
-    def xoa_tu(self, ma_tu):
-        for tu in self.danh_sach_tu:
-            if tu.ma_tu == ma_tu:
-                self.danh_sach_tu.remove(tu)
-                break
+    def tinh_tong_so_tu_trong(self):
+        return len(self.danh_sach_tu_trong)
+
+    def tinh_tong_so_tu_da_dat(self):
+        return len(self.danh_sach_tu_da_dat)
 
 
 class Khachhang(QuanLyTuDo):  # Lop Khach hang
     def __init__(self, ten_dang_nhap, mat_khau):
-        super().__init__(danh_sach_tu)
+        super().__init__()
         self.ten_dang_nhap = ten_dang_nhap
         self.mat_khau = mat_khau
         self.tu_da_chon = None
         self.start_time = None
         self.end_time = None
         self.thoi_gian_su_dung = None
-
 
     def login(self):  # Ham dang nhap
         # Kiểm tra tên đăng nhập
@@ -66,15 +80,19 @@ class Khachhang(QuanLyTuDo):  # Lop Khach hang
             return 1
 
 
-tu1 = Tu("A1", "trong")
-tu2 = Tu("A2", "trong")
-tu3 = Tu("A3", "trong")
-tu4 = Tu("A4", "trong")
-tu5 = Tu("A5", "trong")
-danh_sach_tu = [tu1, tu2, tu3, tu4, tu5]
-quan_ly_tu_do = QuanLyTuDo(danh_sach_tu)
-quan_ly_tu_do.hien_thi_so_tu_con_trong()
-quan_ly_tu_do.hien_thi_tu_trong()
-
-
-
+# Giả sử đưa vào ban đầu tất cả các tủ đều trống
+tu1 = Tu("A1", "trống")
+tu2 = Tu("A2", "trống")
+tu3 = Tu("A3", "trống")
+tu4 = Tu("A4", "trống")
+tu5 = Tu("A5", "trống")
+# Tạo một đối tượng QuanLyTuDo
+quan_ly_tu_do = QuanLyTuDo()
+# Thêm các đối tượng Tu vào danh sách tủ trống
+quan_ly_tu_do.them_tu_trong(tu1)
+quan_ly_tu_do.them_tu_trong(tu2)
+quan_ly_tu_do.them_tu_trong(tu3)
+quan_ly_tu_do.them_tu_trong(tu4)
+quan_ly_tu_do.them_tu_trong(tu5)
+# Tính tổng số tủ trống
+print(quan_ly_tu_do.tinh_tong_so_tu_trong())
