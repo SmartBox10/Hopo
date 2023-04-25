8ffbe0cf859a0ca07cf7c1cce9c7b56374d5271b
LE NGUYEN NGOC LINH <118876372+LINHLNN24042004@users.noreply.github.com>
2023-04-25 22:49:40 +0700

    checkhandle.py

--- checkhandle.py
+++ checkhandle.py
@@ -3,21 +3,3 @@ from check import Ui_MainWindow
 class CHECK_HANDLE(Ui_MainWindow):
     def __init__(self,mainwindow):
         self.setupUi(mainwindow)
-        #----------------------
-        self.plainTextEdit.setPlainText("NHAP MA")
-
-
-    def ham(self):
-        code = self.plainTextEdit.toPlainText()
-        if code == str(12345):
-            return 0
-
-        else:
-            return 1
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = MAIN_HANDLE()
-    MainWindow.show()
-    sys.exit(app.exec_())
